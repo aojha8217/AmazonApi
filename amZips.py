00720164cd79fbb7cc0713b59610273cce2315d1
Ashwin Ojha <aojha@umich.edu>
2018-02-22 20:38:27 -0500

    Python

--- amZips.py
+++ amZips.py
@@ -11,11 +11,37 @@ import operator
 
 
 
+
+
+
+def parse(inputString, finalDict):
+	
+	entryParts = inputString.split(" : ")
+
+
+	key = entryParts[0].strip()
+	finalDict[key] = {}
+	finalDict[key]['Date'] = (entryParts[1].strip())
+	finalDict[key]['Asin'] = (entryParts[2].strip())
+	finalDict[key]['Name'] = (entryParts[3].strip())
+
+	finalDict[key]['Address'] = entryParts[4].split(",")[1].strip("\n").strip()
+	return finalDict
+
+
+
+
+
 if __name__ == "__main__":
 	filename = "Zipcodes.txt"
+	finalDict = {}
 	inputFile = open(filename,'r')
-	data = inputFile.readlines()
-	for x in range(len(data)):
-		data[x] = data[x].strip()
-	print(data)
+	data = inputFile.readlines()#Stores each line as an entry into the array data
+	for x in data:
+		finalDict = parse(x,finalDict)
+
+	
+	
+
+
 	
